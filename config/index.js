b4e9d84410300645990a479ad9690a82d1b7c062
Carlo Nazareno <csnazareno94@gmail.com>
2020-03-12 20:57:29 -0700

    config change

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect('mongodb://localhost/workout', {
+module.exports = require('mongoose').connect(MONGODB_URI ? MONGODB_URI : 'mongodb://localhost/workout', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
