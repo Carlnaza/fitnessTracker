52140d6094b462ee89414288fe133a47d2bf13af
Carlo Nazareno <csnazareno94@gmail.com>
2020-03-12 21:08:58 -0700

    changed config aria broke my config

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect(MONGODB_URI ? MONGODB_URI : 'mongodb://localhost/workout', {
+module.exports = require('mongoose').connect('mongodb://heroku_xw83zwks:u0fq5srk8ripltmmru86lcf3qh@ds111565.mlab.com:11565/heroku_xw83zwks', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
